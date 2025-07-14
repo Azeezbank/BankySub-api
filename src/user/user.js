14f3d134c5984640e6b87a08ef9c83773034da6e
Azeezbank <bankoleazeezb98@gmail.com>
2025-07-14 16:50:40 +0100

    updated

--- src/user/user.js
+++ src/user/user.js
@@ -16,7 +16,7 @@ const router = express.Router();
 
 
 // Fetch User Details
-router.get("/:page/:limit", (req, res) => {
+router.get("/", (req, res) => {
   const page = parseInt(req.query.page) || 1;
   const limit = parseInt(req.query.limit) || 10;
   const offset = (page - 1) * limit;
