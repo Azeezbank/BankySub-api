ebb3df2ca003676ac9888143db602ff2de440083
Azeezbank <bankoleazeezb98@gmail.com>
2025-07-22 16:48:19 +0100

    updated

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,8 +1,8 @@
 const corsOptions = {
-  origin: (origin, callback) => {
-    callback(null, true);
-  },
-  // origin: '*',
+  // origin: (origin, callback) => {
+  //   callback(null, true);
+  // },
+  origin: true,
   methods: ["POST", "GET", "DELETE", "PUT", 'HEAD', 'PATCH',],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
