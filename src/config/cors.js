740e15f3d1d76bd34902b285c689caaffc6c2822
Azeezbank <bankoleazeezb98@gmail.com>
2025-07-22 20:02:58 +0100

    updated

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,11 +1,19 @@
+const allowedOrigins = [
+  "https://bankysub.vercel.app/",
+  "http://localhost:5173/",
+];
+
 const corsOptions = {
-  // origin: (origin, callback) => {
-  //   callback(null, true);
-  // },
-  origin: true,
-  methods: ["POST", "GET", "DELETE", "PUT", 'HEAD', 'PATCH',],
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["POST", "GET", "DELETE", "PUT", "HEAD", "PATCH"],
   credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 };
 
 export default corsOptions;
