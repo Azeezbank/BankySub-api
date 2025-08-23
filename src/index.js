12d65e7d2e2eef843af6565fd90819ea8622682a
Banky <159575592+Azeezbank@users.noreply.github.com>
2025-08-23 19:27:15 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -63,6 +63,6 @@ app.use('/api/monnify', authenticateToken, monnify);
 
 
 //Connection port
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
