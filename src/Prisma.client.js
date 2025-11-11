b239c098934e30ef15cd0501c4699e3b62eca811
Banky <159575592+Azeezbank@users.noreply.github.com>
2025-11-11 15:30:35 +0100

    Update Prisma.client.js

--- src/Prisma.client.js
+++ src/Prisma.client.js
@@ -4,7 +4,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
   datasources: { db: { url: process.env.DATABASE_URL } },
-  log: ["query", "info", "warn", "error"],
+  log: ["warn", "error"],
 });
 
 
